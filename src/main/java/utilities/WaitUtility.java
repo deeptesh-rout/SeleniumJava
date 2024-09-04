20d3578ce91dc116492988d7be658aefb0fad414
deeptesh-rout <153610471+deeptesh-rout@users.noreply.github.com>
2024-09-04 20:24:53 +0530

    first commit

---
+++ src/main/java/utilities/WaitUtility.java
@@ -0,0 +1,29 @@
+package utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitUtility extends Utility {
+
+  public static void explicitWaitUntilVisible(int seconds, By locator) {
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+    wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+  }
+
+  @SuppressWarnings("unchecked")
+  public static void fluentWaitUntilVisible(int seconds, By locator) {
+    @SuppressWarnings("rawtypes")
+    FluentWait fluentWait = new FluentWait(driver)
+            .withTimeout(Duration.ofSeconds(seconds))
+            .pollingEvery(Duration.ofMillis(500))
+            .ignoring(NoSuchElementException.class,
+                    StaleElementReferenceException.class);
+    fluentWait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+  }
+}
